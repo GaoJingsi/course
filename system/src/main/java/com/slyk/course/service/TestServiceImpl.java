286275695f5970813ee944ee6118846134f82c8f
GaoJingsi <jingsi_leo@foxmail.com>
2020-04-29 15:07:23 +0800

    1. 集成spring-boot-devtools(需要开启settings-Compiler-Build project automatically，以及双击shift键，搜索"registry"，点击Actions-Registry，进入界面后勾选compiler.automake.allow.when.app.running选项) 2. 自定义日志格式

--- system/src/main/java/com/slyk/course/service/TestServiceImpl.java
+++ system/src/main/java/com/slyk/course/service/TestServiceImpl.java
@@ -1,6 +1,8 @@
 package com.slyk.course.service;
 
 import com.slyk.course.mapper.TestMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -10,10 +12,13 @@ import java.util.Map;
 @Service
 public class TestServiceImpl implements TestService {
 
+    private Logger log = LoggerFactory.getLogger(TestServiceImpl.class);
+
     @Resource
     private TestMapper testMapper;
 
     public List<Map<String, Object>> test() {
+        log.info("测试热部署");
         return testMapper.test();
     }
 }
